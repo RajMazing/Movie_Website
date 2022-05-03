05a5a34be96d957845042eaa1020afa3db00b979
RajMazing <caseyraj92@gmail.com>
2022-05-03 18:38:42 -0500

    clicking the right arrow 3 times will et the movie menu list revert back to the start

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
 arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
     arrow.addEventListener("click",() => {
-        movieLists[i].style.transform = "translateX(-100px)"
+      clickCounter++;
+
+      if (itemNumber - (4 + clickCounter) >= 0){
+        movieLists[i].style.transform = `translateX(${
+          movieLists[i].computedStyleMap().get("transform")[0].x.value
+        - 300}px)`;
+      } else {
+           movieLists[i].style.transform = "translateX(0)";
+           clickCounter++;
+      }
     })
+  console.log(movieLists[i].querySelectorAll("img").length);
 })
