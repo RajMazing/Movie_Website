0bd5fe261ed24f6214b18ae371edfb6a569282de
RajMazing <caseyraj92@gmail.com>
2022-05-03 19:22:09 -0500

    navbar toggle can now switch between dark and light modes

--- app.js
+++ app.js
@@ -18,3 +18,13 @@ arrows.forEach((arrow, i) => {
     })
   console.log(movieLists[i].querySelectorAll("img").length);
 })
+
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container, .movie-list-title, .navbar-container, .sidebar, .left-menu-icon, .toggle")
+
+ball.addEventListener("click", () => {
+  items.forEach(item => {
+          item.classList.toggle("active")
+  })
+})
