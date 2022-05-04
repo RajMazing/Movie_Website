00ad53a2cfa29e76c574eb5e5455e5aa1cb59e6c
RajMazing <caseyraj92@gmail.com>
2022-05-03 20:47:15 -0500

    completed slider functionality

--- app.js
+++ app.js
@@ -5,19 +5,26 @@ arrows.forEach((arrow, i) => {
   const itemNumber = movieLists[i].querySelectorAll("img").length;
   let clickCounter = 0;
     arrow.addEventListener("click",() => {
+      const ratio = Math.floor(window.innerWidth/270);
       clickCounter++;
 
-      if (itemNumber - (4 + clickCounter) >= 0){
+      if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0){
         movieLists[i].style.transform = `translateX(${
           movieLists[i].computedStyleMap().get("transform")[0].x.value
         - 300}px)`;
       } else {
            movieLists[i].style.transform = "translateX(0)";
-           clickCounter++;
+           clickCounter = 0;
       }
     })
-  console.log(movieLists[i].querySelectorAll("img").length);
+    
+  console.log();
 })
+
+
+
+//TOGGLE
+
 
 
 const ball = document.querySelector(".toggle-ball");
@@ -27,4 +34,6 @@ ball.addEventListener("click", () => {
   items.forEach(item => {
           item.classList.toggle("active")
   })
+
+  ball.classList.toggle("active");
 })
